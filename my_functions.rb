4615516e534fba1bad6b473a9bac7722c369b99b
denkoroku <cathriddoch@gmail.com>
2019-09-04 15:15:43 +0100

    forgetting to init git

---
+++ my_functions.rb
@@ -0,0 +1,22 @@
+def add_array_lengths (array1, array2)
+  array1_length = array1.length
+  array2_length = array2.length
+  return array1_length + array2_length
+end
+
+def sum_array (array)
+  return array.sum
+end
+
+def is_item_in_array (array, item)
+    array.each do |n|
+      if n == item
+      else
+        return false
+      end
+  end
+end
+
+def get_first_key (hash)
+  return hash.first
+end
